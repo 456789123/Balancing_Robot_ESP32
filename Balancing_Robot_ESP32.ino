103e45cdcb6d37aa240cc928483ccf02c677ae44
Christiano Reis <lestedy@gmail.com>
2025-05-04 21:35:21 -0300

    Implementation of a small robot that balances on two wheels

--- Balancing_Robot_ESP32.ino
+++ Balancing_Robot_ESP32.ino
@@ -1,6 +1,5 @@
 /*
   https://github.com/mahowik/BalancingWii/tree/master
-
 */
 
 #include <Wire.h>
@@ -21,7 +20,7 @@
 
 #define SETPOINT 0 // target angle (upright)
 #define INTEGRAL_MAX 1000
-#define DEAD_ZONE 3.5  // zona morta
+#define DEAD_ZONE 0.5  // zona morta
 
 double angle, gyroMicros;
 
@@ -56,18 +55,17 @@ void readMPU() {
   double angleAcc = atan2(-yAcc, zAcc) * RAD_TO_DEG;
 
   angle = (0.9 * (angle + angleGyro) + 0.1 * (angleAcc)) - SETPOINT;
-
 }
 
 void calculate_PID() {
   double error = -angle;
-
+/*
   if (abs(error) < DEAD_ZONE) {
     pidOutput = 0;
     integral = 0;
     return;
   }
-
+*/
   integral += error;
   if (integral > INTEGRAL_MAX) integral = INTEGRAL_MAX;
   if (integral < -INTEGRAL_MAX) integral = -INTEGRAL_MAX;
@@ -119,17 +117,13 @@ void loop() {
     int motorSpeed = constrain(abs(pidOutput), 0, 800);
     bool direction = (pidOutput > 0);
 
-    // Set direction
     digitalWrite(DIR_RIGHT, direction ? LOW : HIGH);
     digitalWrite(DIR_LEFT, direction ? HIGH : LOW);
 
-    if (motorSpeed > 0) {
-      // INVERTED MAP: higher motorSpeed → smaller delay → faster pulses
-      int pulseDelay = map(motorSpeed, 0, 800, 870, 200);  // adjust as needed
+    int steps = map(motorSpeed, 0, 800, 0, 50);  // ajusta 50 conforme necessário
+    int pulseDelay = map(motorSpeed, 0, 800, 870, 200);  // adjust as needed
 
-      // Minimum delay cap (avoid too fast)
-      //if (pulseDelay < 200) pulseDelay = 200;
-
+    for (int i = 0; i < steps; i++) {
       digitalWrite(STEP_LEFT, HIGH);
       digitalWrite(STEP_RIGHT, HIGH);
       delayMicroseconds(pulseDelay);
@@ -139,5 +133,3 @@ void loop() {
     }
   }
 }
-
-
